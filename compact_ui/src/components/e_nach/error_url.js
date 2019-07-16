672f5f984130bc611bba88ba99cb8a0af2734ecf
Santosh D <sgdevmane@gmail.com>
2019-07-16 19:38:13 +0530

    UI changes for eNACH

--- compact_ui/src/components/e_nach/error_url.js
+++ compact_ui/src/components/e_nach/error_url.js
@@ -1,38 +1,38 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {Link, withRouter} from "react-router-dom";
-import {changeLoader, EnachsetAttempt, EnachsetPayload} from "../../actions";
+import React from "react";
+import { connect } from "react-redux";
+import {  withRouter } from "react-router-dom";
+import { changeLoader, EnachsetAttempt, EnachsetPayload } from "../../actions";
 
+const Error_URL = props => {
+  return (
+    <>
+      <i
+        className="fa fa-times-circle checkCircle"
+        style={{ color: "red", fontSize: "50px" }}
+      />
+      <h5 className={"text-center"}> E-NACH Failed !</h5>
+      <br />
 
-class Error_URL extends Component {
-
-    render() {
-        return (
-            <>
-                <i className="fa fa-times-circle checkCircle" style={{color:'red'}}></i>
-                <h3 className={"text-center"}> E-NACH Failed !</h3>
-                <br/>
-
-                <div className="alert alert-danger" role="alert">
-                    {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
-                    <p className="paragraph_styling  ">
-                        Thank you for completing the process, For some reason we couldn't complete the e-NACH, you'll be
-                        redirected to Anchor dashboard within a moment..
-                    </p>
-                </div>
-            </>)
-    }
-
-}
+      <div className="alert alert-danger" role="alert">
+        {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
+        <p className="paragraph_styling  ">
+          Thank you for completing the process, For some reason we couldn't
+          complete the e-NACH, you'll be redirected to Anchor dashboard within a
+          moment..
+        </p>
+      </div>
+    </>
+  );
+};
 
 const mapStateToProps = state => ({
-    token: state.eNachReducer.token,
-    eNachPayload: state.eNachReducer.eNachPayload,
+  token: state.eNachReducer.token,
+  eNachPayload: state.eNachReducer.eNachPayload
 });
 
 export default withRouter(
-    connect(
-        mapStateToProps,
-        {changeLoader, EnachsetPayload, EnachsetAttempt}
-    )(Error_URL)
+  connect(
+    mapStateToProps,
+    { changeLoader, EnachsetPayload, EnachsetAttempt }
+  )(Error_URL)
 );
--- compact_ui/src/components/e_nach/success_url.js
+++ compact_ui/src/components/e_nach/success_url.js
@@ -1,27 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { changeLoader, EnachsetAttempt, EnachsetPayload } from "../../actions";
 
-class Success_URL extends Component {
-  render() {
-    return (
-      <>
-        <i className={"fa fa-check-circle checkCircle"} />
-        <h3 className={"text-center"}> E-NACH Completed !</h3>
-        <br />
+const Success_URL = props => {
+  return (
+    <>
+      <i
+        className={"fa fa-check-circle checkCircle"}
+        style={{ fontSize: "50px" }}
+      />
+      <h5 className={"text-center"}> E-NACH Completed !</h5>
+      <br />
 
-        <div className="alert alert-success" role="alert">
-          {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
-          <p className="paragraph_styling  ">
-            Thank you for completing the E-NACH process, you'll be redirected to
-            Anchor dashboard within a moment..
-          </p>
-        </div>
-      </>
-    );
-  }
-}
+      <div className="alert alert-success" role="alert">
+        {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
+        <p className="paragraph_styling  ">
+          Thank you for completing the E-NACH process, you'll be redirected to
+          Anchor dashboard within a moment..
+        </p>
+      </div>
+    </>
+  );
+};
 
 const mapStateToProps = state => ({
   token: state.eNachReducer.token,
