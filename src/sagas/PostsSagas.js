5c8b39f78594a2f15abf871181f204f6a584e55d
McTAVISH <sgdevmane@gmail.com>
2019-09-14 20:07:44 +0530

    clutter removal

--- src/sagas/PostsSagas.js
+++ src/sagas/PostsSagas.js
@@ -9,9 +9,6 @@ import {
 } from "../shared/CacheLogic.js";
 import {gst_karza, test_kscan, karza_key, otpUrl} from "../shared/constants";
 
-const ROOT_URL = "//api.themoviedb.org/3";
-const API_KEY = "?api_key=583f5418d3702c0709d79b38812b7e7c";
-
 // Watcher sagas
 export function* watchFetchAuth() {
     yield takeEvery(types.FETCH_AUTH, workFetchAuth);
@@ -25,13 +22,6 @@ export function* watchVerifyOTP() {
     yield takeEvery(types.RECEIVE_OTP, workVerifyOTP);
 }
 
-export function* watchFetchPosts() {
-    yield takeEvery(types.FETCH_POSTS, workFetchPosts);
-}
-
-export function* watchFetchUpcoming() {
-    yield takeLatest(types.FETCH_UPCOMING, workFetchUpcome);
-}
 
 // Worker sagas
 
@@ -66,51 +56,17 @@ export function* workVerifyOTP({otp_id}) {
 }
 
 export function* workFetchAuth({otp, token}) {
-    let config = {
-        headers: {Authorization: "bearer " + token}
-    };
-    try {
-        const uri = encodeURI(`${ROOT_URL}/otp/verify/${API_KEY}`);
-        const response = yield call(axios.post, uri, {otp: otp}, config);
-        yield put({
-            type: types.FETCH_AUTH_SUCCESS,
-            payload: response.data.results
-        });
-    } catch (error) {
-        console.log("Request failed! Could not fetch Payload.");
-    }
-}
-
-export function* workFetchPosts({query}) {
-    try {
-        const uri = encodeURI(`${ROOT_URL}/search/movie${API_KEY}&query=${query}`);
-        const response = yield call(axios.get, uri);
-
-        yield put({
-            type: types.FETCH_POSTS_SUCCESS,
-            payload: response.data.results
-        });
-    } catch (error) {
-        console.log("Request failed! Could not fetch posts.");
-    }
-}
-
-export function* workFetchUpcome() {
-    try {
-        const uri = encodeURI(`${ROOT_URL}/movie/upcoming${API_KEY}`);
-        let response = {data: []};
-
-        if (getObject(uri) === null && !isCached(uri)) {
-            response = yield call(axios.get, uri);
-            setCached(uri);
-            setObject(uri, response.data);
-        } else response.data = getObject(uri);
-
-        yield put({
-            type: types.FETCH_UPCOMING_SUCCESS,
-            payload: response.data.results
-        });
-    } catch (error) {
-        console.log("Request failed! Could not fetch posts.");
-    }
+    /* let config = {
+         headers: {Authorization: "bearer " + token}
+     };
+     try {
+         const uri = encodeURI(`${ROOT_URL}/otp/verify/${API_KEY}`);
+         const response = yield call(axios.post, uri, {otp: otp}, config);
+         yield put({
+             type: types.FETCH_AUTH_SUCCESS,
+             payload: response.data.results
+         });
+     } catch (error) {
+         console.log("Request failed! Could not fetch Payload.");
+     }*/
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,9 @@ import "regenerator-runtime/runtime";
 import {
     watchSendOTP,
     watchVerifyOTP,
-    watchFetchPosts,
-    watchFetchUpcoming
 } from "./PostsSagas";
 
 // Root sagas
 export default function* rootSaga() {
-    yield [watchSendOTP(), watchVerifyOTP(), watchFetchPosts(), watchFetchUpcoming()];
+    // yield [watchSendOTP(), watchVerifyOTP()];
 }
