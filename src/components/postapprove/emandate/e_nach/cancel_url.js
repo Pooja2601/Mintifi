da03495ac90c2eaa08c0faaa3b2d8c87a21c7f0d
Santosh D <sgdevmane@gmail.com>
2019-09-19 13:46:38 +0530

    url modification

--- src/components/postapprove/emandate/e_nach/cancel_url.js
+++ src/components/postapprove/emandate/e_nach/cancel_url.js
@@ -1,56 +1,59 @@
 import React from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { changeLoader, EnachsetAttempt, EnachsetPayload } from "../../../../actions";
-import { postMessage } from "../../../../shared/common_logic";
+import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
+import {changeLoader, EnachsetAttempt, EnachsetPayload} from "../../../../actions";
+import {postMessage} from "../../../../shared/common_logic";
 import PropTypes from "prop-types";
 
-const Cancel_URL = props => {
-  if (window.location !== window.parent.location) {
-    postMessage({
-      enach_status: "cancel",
-      action: "close",
-      loan_id: props.eNachPayload.loan_application_id
-    });
-  } else {
-    window.setTimeout(() => {
-      window.location.href = `${props.eNachPayload.cancel_url}`;
-    }, 4000);
-  }
+const PUBLIC_URL = process.env;
+const Cancel_URL = props => {l
+    const hosty = props.eNachPayload.cancel_url.localeCompare(PUBLIC_URL);
+    if (window.location !== window.parent.location) {
+        postMessage({
+            enach_status: "cancel",
+            action: "close",
+            loan_id: props.eNachPayload.loan_application_id
+        });
+    } else {
+        if (hosty === -1) // URLs and HOST are same
+            window.setTimeout(() => {
+                window.location.href = `${props.eNachPayload.cancel_url}`;
+            }, 4000);
+    }
 
-  return (
-    <>
-      <i
-        className={"fa fa-exclamation checkCircle"}
-        style={{ color: "orange" }}
-      />
-      <h3 className={"text-center"}> E-NACH Pending/Cancelled !</h3>
-      <br />
+    return (
+        <>
+            <i
+                className={"fa fa-exclamation checkCircle"}
+                style={{color: "orange"}}
+            />
+            <h3 className={"text-center"}> E-NACH Pending/Cancelled !</h3>
+            <br/>
 
-      <div className="alert alert-warning" role="alert">
-        {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
-        <p className="paragraph_styling  ">
-          We cannot process the eNACH Application as of now, you'll be
-          redirected to Anchor dashboard within a moment, you may try again from
-          Anchor's portal..
-        </p>
-      </div>
-    </>
-  );
+            <div className="alert alert-warning" role="alert">
+                {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
+                <p className="paragraph_styling">
+                    We cannot process the eNACH Application as of now, {!hosty && `you'll be
+                    redirected to Anchor dashboard within a moment,`} you may try again from
+                    Merchant's portal..
+                </p>
+            </div>
+        </>
+    );
 };
 
 Cancel_URL.propTypes = {
-  eNachPayload: PropTypes.object.isRequired
+    eNachPayload: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  token: state.eNachReducer.token,
-  eNachPayload: state.eNachReducer.eNachPayload
+    token: state.eNachReducer.token,
+    eNachPayload: state.eNachReducer.eNachPayload
 });
 
 export default withRouter(
-  connect(
-    mapStateToProps,
-    { changeLoader, EnachsetPayload, EnachsetAttempt }
-  )(Cancel_URL)
+    connect(
+        mapStateToProps,
+        {changeLoader, EnachsetPayload, EnachsetAttempt}
+    )(Cancel_URL)
 );
--- src/components/postapprove/emandate/e_nach/error_url.js
+++ src/components/postapprove/emandate/e_nach/error_url.js
@@ -4,19 +4,21 @@ import {withRouter} from "react-router-dom";
 import {changeLoader, EnachsetAttempt, EnachsetPayload} from "../../../../actions";
 import {postMessage} from "../../../../shared/common_logic";
 import PropTypes from "prop-types";
-
+const PUBLIC_URL = process.env;
 const Error_URL = props => {
+    const hosty = props.eNachPayload.success_url.localeCompare(PUBLIC_URL);
     if (window.location !== window.parent.location)
         postMessage({
             enach_status: "error",
             action: "close",
             loan_id: props.eNachPayload.loan_application_id
         });
-    else
-        window.setTimeout(() => {
-            window.location.href = `${props.eNachPayload.error_url}`;
-        }, 4000);
-
+    else {
+        if (hosty === -1) // URLs and HOST aren't same
+            window.setTimeout(() => {
+                window.location.href = `${props.eNachPayload.error_url}`;
+            }, 4000);
+    }
     return (
         <>
             <i className="fa fa-times-circle checkCircle" style={{color: "red"}}/>
@@ -25,10 +27,10 @@ const Error_URL = props => {
 
             <div className="alert alert-danger" role="alert">
                 {/*<h4 className="alert-heading">Dear {f_name} {l_name}</h4>*/}
-                <p className="paragraph_styling  ">
+                <p className="paragraph_styling">
                     Thank you for completing the process, For some reason we couldn't
-                    complete the e-NACH, you'll be redirected to Anchor dashboard within a
-                    moment..
+                    complete the e-NACH {!hosty && `, you'll be redirected to Anchor dashboard within a
+                    moment..`}
                 </p>
             </div>
         </>
