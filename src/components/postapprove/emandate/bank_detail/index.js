0213480897c7d49f17a37db9490d0500f5944c9e
Muskan <muskan.jain@mintifi.com>
2019-09-27 19:28:49 +0530

    fix issues

--- src/components/postapprove/emandate/bank_detail/index.js
+++ src/components/postapprove/emandate/bank_detail/index.js
@@ -356,7 +356,7 @@ class ENachBankDetail extends Component {
             {" "}
             Please submit your bank account details for loan disbursement.
             Please note that same bank account will be used to set up
-            auto-debit/standing instructions for repayment of your loan amount.
+            auto-debit/standing instructions for repayment :
           </b>
         </p>
         {/*<h5 className={"text-center"}>{(gstProfile === Object(gstProfile)) ? gstProfile.lgnm : ''}</h5>*/}
